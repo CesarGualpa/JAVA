f307dd0879193e033d996dc7a75072ecb85a7ec1
CesarGualpa <cesar_amagu@hotmail.com>
2026-07-28 16:36:03 -0500

    feat: clase Cuadrado con area y perimetro

---
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,55 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		
+		Cuadrado c1 = new Cuadrado();
+		c1.lado = 4;
+		
+		double resultadoArea1;
+		resultadoArea1 = c1.calcularArea();
+		
+		double resultadoPerimetro1;
+		resultadoPerimetro1 = c1.calcularPerimetro();
+		
+		System.out.println("Cuadrado 1");
+		System.out.println("Lado: " + c1.lado);
+		System.out.println("Area: " + resultadoArea1);
+		System.out.println("Perimetro: " + resultadoPerimetro1);
+		
+		System.out.println();
+		
+		Cuadrado c2 = new Cuadrado();
+		c2.lado = 3;
+		
+		double resultadoArea2;
+		resultadoArea2 = c2.calcularArea();
+		
+		double resultadoPerimetro2;
+		resultadoPerimetro2 = c2.calcularPerimetro();
+		
+		System.out.println("Cuadrado 2");
+		System.out.println("Lado: " + c2.lado);
+		System.out.println("Area: " + resultadoArea2);
+		System.out.println("Perimetro: " + resultadoPerimetro2);
+		
+		System.out.println();
+		
+		Cuadrado c3 = new Cuadrado();
+		c3.lado = 5;
+		
+		double resultadoArea3;
+		resultadoArea3 = c3.calcularArea();
+		
+		double resultadoPerimetro3;
+		resultadoPerimetro3 = c3.calcularPerimetro();
+		
+		System.out.println("Cuadrado 3");
+		System.out.println("Lado: " + c3.lado);
+		System.out.println("Area: " + resultadoArea3);
+		System.out.println("Perimetro: " + resultadoPerimetro3);
+
+	}
+
+}
